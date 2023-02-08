a7ae9f2eba641b74a6ba8c8dbcab271e6426da07
Maurício Braz Gonçalves <unbluew@gmail.com>
2023-02-08 10:24:32 -0400

    ci: configure husky and lint-staged

---
+++ apps/web/.lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+function buildEslintCommand(filenames) {
+  return `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+}
+
+module.exports = {
+  "**/*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};
